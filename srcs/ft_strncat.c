b25aaf056c14e011c5fd6b735f65cf5e23a2ed96
mickberg <mikael.berglund2@gmail.com>
2019-11-08 23:18:15 +0200

    ft_strncat fix tests

--- srcs/ft_strncat.c
+++ srcs/ft_strncat.c
@@ -15,18 +15,21 @@ int	main(void)
 	char	*name = "ft_strncat";
 
 	char	str1[40] = "Leading string";
+	char	_str1[40] = "Leading string";
 	char	*str2 = ", trailing";
 	char	base[30] = "One";
+	char	_base[30] = "One";
 	char	*a = "Two";
 	char	*b = "Three";
 	char	*c = "Four";
 	char	empty[11] = "";
+	char	_empty[11] = "";
 	char	*fill = "ten length";
 
-	test_str(strncat(str1, str2, strlen(str2)/2), ft_strncat(str1, str2, strlen(str2)/2), name, "simple test");
-	test_str(strncat(base, a, 3), ft_strncat(base, a,3), name, "base -> a");
-	test_str(strncat(base, b, 1), ft_strncat(base, b, 1), name, "base -> b");
-	test_str(strncat(base, c, 4), ft_strncat(base, c, 4), name, "base -> c");
-	test_str(strncat(empty, fill, 10), ft_strncat(empty, fill, 10), name, "from empty");
+	test_str(strncat(_str1, str2, strlen(str2)/2), ft_strncat(str1, str2, strlen(str2)/2), name, "simple test");
+	test_str(strncat(_base, a, 3), ft_strncat(base, a,3), name, "base -> a");
+	test_str(strncat(_base, b, 1), ft_strncat(base, b, 1), name, "base -> b");
+	test_str(strncat(_base, c, 4), ft_strncat(base, c, 4), name, "base -> c");
+	test_str(strncat(_empty, fill, 10), ft_strncat(empty, fill, 10), name, "from empty");
 	return (0);
 }
