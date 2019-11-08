73d36cf9cc8cfba76be007571c3580d50350c712
mickberg <mikael.berglund2@gmail.com>
2019-11-08 21:19:51 +0200

    ft_memmove improved tests

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -13,33 +13,30 @@ int	main(void)
 
 	char	*name = "ft_memmove";
 
-	char	*exp = (char*)malloc(sizeof(char) * 15);
-	if (!exp)
-		return (1000);
-	memset(exp, 0, 15);
+	char	*exp;
+	char	*test;
+	size_t	size;
 
-	char	*test = (char*)malloc(sizeof(char) * 15);
-	if (!test)
-		return (1000);
-	memset(test, 0, 15);
+	exp = strdup("kalle anka pelle");
+	test = strdup("kalle anka pelle");
+	size = strlen(exp) + 1;
+	test_mem(memmove(exp, exp+5, 10), ft_memmove(test, test+5, 10), size, name, "test1");
 
-	memset(exp, 'f', 12);
-	memset(test, 'f', 12);
-	test_mem(memmove(exp, "test string", 11), ft_memmove(test, "test string", 11), 15, name, "exact len");
-
-	memset(exp, 0, 15);
-	memset(test, 0, 15);
-	test_mem(memmove(exp, "test string", 0), ft_memmove(test, "test string", 0), 15, name, "0 len");
+	exp = strdup("kalle anka pelle");
+	test = strdup("kalle anka pelle");
+	test_mem(memmove(exp+5, exp, 10), ft_memmove(test+5, test, 10), size, name, "test2");
 
 
+	exp = strdup("kalle anka pelle");
+	test = strdup("kalle anka pelle");
+	test_mem(memmove(exp+5, exp+4, 10), ft_memmove(test+5, test+4, 10), size, name, "test3");
+
+	/* corner case test */
 	size_t	size128 =  128 * 1024 * 1024;
 	char	*test128 = ft_strnew(size128);
 	char	*exp128 = ft_strnew(size128);
-
 	memset(exp128, 'F', size128);
-
 	test_mem(exp128, ft_memmove(test128, exp128, size128), size128, name, "128mb");
-
 
 	return (0);
 }
