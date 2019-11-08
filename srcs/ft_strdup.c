6399bd5dbce8e6c2d2cec53752a231ba830c85ca
mickberg <mikael.berglund2@gmail.com>
2019-11-08 22:17:51 +0200

    ft_strdup improve tests

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 16:39:07 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/02 21:09:45 by mikaelber        ###   ########.fr       */
+/*   Updated: 2019/11/08 22:17:22 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ int	main(void)
 
 	char	*name = "ft_strdup";
 
-	char	*str1 = "Kalle anka";
+	char	*str1 = "Kalle anka pelle";
 	char	*str2 = "kalle\0anka";
 	char	*str3 = "";
+	int		test[5] = {1819042123, 1851859045, 1881170283, 1701604453, 0};
 
-	test_str(strdup(str1), ft_strdup(str1), name, "normal");
-	test_str(strdup(str2), ft_strdup(str2), name, "terminator in middle");
-	test_str(strdup(str3), ft_strdup(str3), name, "empty");
+	test_mem(strdup(str1), ft_strdup(str1), strlen(str1) + 1, name, "normal");
+	test_mem(strdup(str2), ft_strdup(str2), 6, name, "terminator in middle");
+	test_mem(strdup(str3), ft_strdup(str3), strlen(str3) + 1, name, "empty");
+	test_mem(strdup((char*)test), ft_strdup((char*)test), 5, name, "int to str");
+
 	return (0);
 }
